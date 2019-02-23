38c35d3a63c2f47d432b3ae3396e7fccabc39241
hujiyuta <hujiyuta@gmail.com>
2019-02-23 11:42:55 +0900

    不要なマイグレーションファイルの削除
    サーバーにdbを導入しようとしたらエラーがでたため、不要なマイグレーションファイルを削除

--- db/migrate/20190119063248_rename_body_column_to_messages.rb
+++
@@ -1,5 +0,0 @@
-class RenameBodyColumnToMessages < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :messages, :body, :content
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190119063248) do
+ActiveRecord::Schema.define(version: 20190114230912) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_groups_on_name", using: :btree
   end
 
   create_table "members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -39,6 +40,7 @@ ActiveRecord::Schema.define(version: 20190119063248) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -46,7 +48,6 @@ ActiveRecord::Schema.define(version: 20190119063248) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
